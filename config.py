8e8301ade1f2852848bdca2d0e04fbfc40a81aa4
zgd <94973326+CabbageSage@users.noreply.github.com>
2024-12-28 21:44:15 +0800

    v2

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 import argparse  
 from torch.cuda import is_available
+
+workers=0
+batch_size=8
+epochs=20
+lr=1e-3
+seed=118
+checkpoint_path=''
+image_size=28
+
 def get_options(parser=argparse.ArgumentParser()):  
   
-    parser.add_argument('--workers', type=int, default=0,  
+    parser.add_argument('--workers', type=int, default=workers,  
                         help='number of data loading workers, you had better put it 4 times of your gpu')  
-    parser.add_argument('--batch_size', type=int, default=8, help='input batch size, default=64')  
-    parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train for, default=10')  
-    parser.add_argument('--lr', type=float, default=1e-3, help='select the learning rate, default=1e-3')  
-    parser.add_argument('--seed', type=int, default=118, help="random seed")  
+    parser.add_argument('--batch_size', type=int, default=batch_size, help='input batch size, default=64')  
+    parser.add_argument('--epochs', type=int, default=epochs, help='number of epochs to train for, default=10')  
+    parser.add_argument('--lr', type=float, default=lr, help='select the learning rate, default=1e-3')  
+    parser.add_argument('--seed', type=int, default=seed, help="random seed")  
     parser.add_argument('--cuda', action='store_true', default=is_available(), help='enables cuda')  
-    parser.add_argument('--checkpoint_path',type=str,default='', help='Path to load a previous trained model if not empty (default empty)')  
+    parser.add_argument('--checkpoint_path',type=str,default=checkpoint_path, help='Path to load a previous trained model if not empty (default empty)')  
     parser.add_argument('--output',action='store_true',default=True,help="shows output")
-    parser.add_argument('--image_size', type=int, default=28, help='input image size, default=64')
+    parser.add_argument('--image_size', type=int, default=image_size, help='input image size, default=64')
   
     opt = parser.parse_args()  
   
